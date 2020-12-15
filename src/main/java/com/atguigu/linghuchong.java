ce7c30d30242fbd814bf21b4463688ae32db133e
zxlong0912 <846833150@qq.com>
2020-12-15 16:45:36 +0800

    update01

---
+++ src/main/java/com/atguigu/linghuchong.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+public class linghuchong {
+    public static void main(String[] args) {
+        System.out.println("笑傲江湖");
+        System.out.println("笑傲江湖");
+        System.out.println("笑傲江湖");
+        System.out.println("笑傲江湖");
+        System.out.println("笑傲江湖");
+        System.out.println("笑傲江湖");
+    }
+}
